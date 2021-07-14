eee6cc0fbb74b31de3dd156a9ef53dc85ea46a0d
merydaniel7 <merydaniel7@gmail.com>
2021-07-14 16:44:22 +0200

    Add: new functions

--- index.js
+++ index.js
@@ -23,11 +23,61 @@ app.get("/api/resources", (req, res) => {
 })
 
 
+app.get("/api/active-resource", (req, res) => {
+    const resources = getResources();
+    const activeResources = resources.find(resource => resource.status === "active");
+    res.send(activeResources)
+})
+
+
 app.get("/api/resources/:id", (req, res) => {
     const resources = getResources();
     const { id } = req.params;
-    const resource = resources.find((item) => item.id === id)
+    const resource = resources.find((item) => item.id === id);
     res.send(resource)
+})
+
+
+app.patch("/api/resources/:id", (req, res) => {
+    const resources = getResources();
+    const { id } = req.params;
+    const resourceIndex = resources.findIndex((item) => item.id === id);
+    const activeResources = resources.find(resource => resource.status === "active")
+
+    if (resources[resourceIndex].status === "complete") {
+        return res.status(422).send("Cannot update because resource has been completed!");
+    }
+
+    resources[resourceIndex] = req.body;
+
+    if (req.body.status === "active") {
+        if (activeResources) {
+            return res.status(422).send("There is active resource already!");
+        }
+
+        resources[resourceIndex].status = "active";
+        resources[resourceIndex].activationTime = new Date();
+    }
+    
+    fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+        if (error) {
+            return res.status(422).send("Cannot store data in the file!");
+        }
+        return res.send("Data has been updated!");
+        })
+})
+
+app.delete("/api/resources/:id", (req, res) => {
+    const resources = getResources();
+    const { id } = req.params;
+    let remainingResources = resources.filter((item) => item.id !== id);
+    
+    fs.writeFile(pathToFile, JSON.stringify(remainingResources, null, 2), (error) => {
+        if (error) {
+            return res.status(422).send("Cannot store data in the file!");
+        }
+        return res.send("Data has been deleted!");
+        })
 })
 
 
